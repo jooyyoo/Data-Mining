05a20e5ef0960266cfb99fabca5e4e252ec04f33
Jooyoung Yun <61531386+jooyyoo@users.noreply.github.com>
2022-11-11 13:45:38 +0900

    Add files via upload

---
+++ datamining.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def add2(number):
+    return number+2
+def mul3(number):
+    return number*3
+def score(number):
+    return "I want the sorce of "+str(number)
+
